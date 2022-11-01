9582d7c53e0f1361f5d65ee463f08ec69d3b3ea3
shin0131 <shinyoung@sookmyung.ac.kr>
2022-11-01 16:47:12 +0900

    03-1

--- main.c
+++ main.c
@@ -7,20 +7,18 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i, average;
-	int sum;
+	int i;
 	int grade[SIZE];
+	int score[SIZE];
 	
-	sum = 0;
 	for(i=0; i<SIZE; i++)
-	{
-		printf("input %i grades : ", SIZE);
-		scanf("%d", &grade[i]);
-		sum += grade[i];
-	}
+		grade[i] = rand() % 100;
 	
-	average = sum / SIZE;
-	printf("aver_grade : %d\n", average);
+	for(i=0; i<SIZE; i++)
+		score[i] = grade[i];
+	
+	for(i=0; i<SIZE; i++)
+		 printf("score[%d] = %d (%d)\n", i , score[i], grade[i]);
 	
 	return 0;
 }
